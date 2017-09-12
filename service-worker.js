3a7a66bf6b7c02661948e4eadd790ea10f53849c
tamerbak <tamer.elbakkali@gmail.com>
2017-09-12 15:51:11 +0200

    Fetch problems..

--- service-worker.js
+++ service-worker.js
@@ -28,39 +28,3 @@ self.toolbox.router.any('/*', self.toolbox.cacheFirst);
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 self.toolbox.router.default = self.toolbox.networkFirst;
-
-self.addEventListener("fetch", function(event) {
-    // If the request in GET, let the network handle things,
-    if (event.request.method !== 'POST') {
-        return;
-    }
-    // here we block the request and handle it our selves
-    event.respondWith(
-        // Returns a promise of the cache entry that matches the request
-        caches
-            .match(event.request)
-            .then(function(response) {
-
-                // here we can hanlde the request how ever we want.
-                // We can reutrn the cache right away if it exisit,
-                // or go to network to fetch it.
-                // There are more intricate examples below.
-                // https://ponyfoo.com/articles/progressive-networking-serviceworker
-
-                if (response) {
-                    // our responce is in the cache, let's return that instead
-                    return response;
-                }
-                // if the responce is not in the cache, let's fetch it
-                return fetch(event.request)
-                    .then(function(response) {
-                        // we have a responce from the network
-                        return response;
-                    }).catch(function(error) {
-                        // Something happened
-                        console.error('Fetching failed:', error);
-                        throw error;
-                    });
-            })
-    );
-});
